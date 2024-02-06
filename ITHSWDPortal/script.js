9a5ac45f2ac7959964393c99ec82e3e0cdb4fb7f
Gregory271 <gregory.rivera11@gmail.com>
2024-02-06 15:34:45 -0500

    Add files via upload
    finalized creation of grade selector buttons on project page, this component will likely be reused on other pages. Initialized and connected script.js into the portfolio page and added script to allow the div for each grade to auto hide until the button is clicked for that grade, then it will display the div with that grades portfolios.

--- ITHSWDPortal/script.js
+++ ITHSWDPortal/script.js
@@ -1,2 +1,28 @@
 // JavaScript Document
 
+let tenButton = document.querySelector(".tenButton");
+let elevenButton = document.querySelector(".elevenButton");
+let twelveButton = document.querySelector(".twelveButton");
+let seniorPortfolios = document.querySelector(".senior-portfolios");
+let juniorPortfolios = document.querySelector(".junior-portfolios");
+let sophomorePortfolios = document.querySelector(".sophomore-portfolios");
+
+tenButton.onclick=function(){
+    console.log("10th grade portfolios selected");
+    sophomorePortfolios.style.display="block";
+    juniorPortfolios.style.display="none";
+    seniorPortfolios.style.display="none";
+    
+}
+elevenButton.onclick=function(){
+    console.log("11th grade portfolios selected");
+    sophomorePortfolios.style.display="none";
+    juniorPortfolios.style.display="block";
+    seniorPortfolios.style.display="none";
+}
+twelveButton.onclick=function(){
+    console.log("12th grade portfolios selected");
+    sophomorePortfolios.style.display="none";
+    juniorPortfolios.style.display="none";
+    seniorPortfolios.style.display="block";
+}
